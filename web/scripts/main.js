0ef71817bd1db03508fb55b71c6ac2d250ea01ee
Nolan Piland <8548667+pieceOpiland@users.noreply.github.com>
2016-12-24 11:21:40 -0500

    Removing Google Analytics

--- web/scripts/main.js
+++ web/scripts/main.js
@@ -1,13 +1,4 @@
 (function(){
-    // Google Analytics here.
-    (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-            (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-        m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-    })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
-
-    ga('create', 'UA-88913516-1', 'auto');
-    ga('send', 'pageview');
-
     // Bootstrap the app here.
     require(["jquery", "TodoList", "bootstrap"], function($, TodoList){
         $(function(){
